0d83a89962aacb15acfe8410945ed01f5c026a81
gabrielppeixoto <gaiel_peixoto@hotmail.com>
2021-04-21 16:24:07 -0300

    Adicionar outras entities, repositorios, migrations

---
+++ src/services/UsersService.ts
@@ -0,0 +1,41 @@
+import { getCustomRepository, Repository } from "typeorm"
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository"
+
+
+
+class UsersService
+{
+    private usersRepository: Repository<User>;
+
+    constructor()
+    {
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+
+    async create(email: string)
+    {
+        
+        //Verificar se usuário existe
+
+        const userExists = await this.usersRepository.findOne({
+            email
+        });
+
+        //se existir, retorna usuario
+        if(userExists)
+        {
+            return userExists;
+        }
+
+        const user = this.usersRepository.create({
+            email,
+        });
+
+        await this.usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export { UsersService }
